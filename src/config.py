db45303f310b0ad48b6ffddc105c6565e11c2ca2
Priyank Kodesia <56622329+priyankkodesianetspi@users.noreply.github.com>
2024-07-06 02:59:57 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -8,5 +8,6 @@ KITE_API_SECRET = "04xnxa3qehzacdggw3evgbhxkk8dse5b"
 KITE_API_KEY = "ye8rerpg2zxmibju"
 PASSPHRASE = "Ivaan@123"
 LOGIN_URL = f"https://kite.zerodha.com/connect/login?v=3&api_key=ye8rerpg2zxmibju"
-REDIRECT_URL = os.getenv("REDIRECT_URL")
+REDIRECT_URL = "https://13.201.92.141/login"
+# REDIRECT_URL="http://127.0.0.1:8000/login"
 MAX_LOSS = os.getenv("MAX_LOSS", 1000)
